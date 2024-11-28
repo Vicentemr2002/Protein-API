1c3146f805d8ea55cc806b3f91b00fd9634f9cba
Jose Vitriago <80923963+Vicentemr2002@users.noreply.github.com>
2024-11-27 21:00:53 -0300

    Completado

--- js/scritp.js
+++ js/scritp.js
@@ -1,3 +1,4 @@
+
 function openNav(){
     document.getElementById("mobile-menu").style.width = "100%";
 }
@@ -5,3 +6,44 @@ function openNav(){
 function closeNav(){
     document.getElementById("mobile-menu").style.width = "0%";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("buscador");
+    const proteinCards = document.querySelectorAll(".index-protein");
+    const noResultsMessage = document.getElementById("noResultsMessage");
+
+
+    function normalizeText(text) {
+        return text
+            .toLowerCase()
+            .replace(/[\s,:.]+/g, " ") 
+            .trim();
+    }
+
+    searchInput.addEventListener("input", function () {
+        const searchValue = normalizeText(searchInput.value);
+        let matches = 0;
+
+        proteinCards.forEach(card => {
+            const cardText = normalizeText(card.textContent);
+
+            if (cardText.includes(searchValue)) {
+                card.style.display = "block"; 
+                matches++;
+            } else {
+                card.style.display = "none"; 
+            }
+        });
+
+    
+        noResultsMessage.style.display = matches > 0 ? "none" : "block";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const proteins = document.querySelectorAll(".index-protein");
+
+    proteins.forEach((protein, index) => {
+        protein.style.animationDelay = `${index * 0.2}s`;
+    });
+});
